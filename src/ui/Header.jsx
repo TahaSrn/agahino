fec50f9b89a87de077e8ed729bc8c3ae2e26a6c6
TahaSrn <tahaahmadi0403@gmail.com>
2026-07-12 15:19:53 +0330

    bugs fixed

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
     <>
       <header className="bg-dark-800/95 fixed top-0 right-0 left-0 z-50 flex w-full items-center justify-center border-b border-white/10 shadow-[0_8px_32px_rgba(0,0,0,0.25)] backdrop-blur-xl transition-all duration-300">
         <div className="flex w-[95%] items-center py-2 md:w-[85%] md:py-0 [&>*:nth-child(1)]:mr-4 md:[&>*:nth-child(1)]:mr-0 [&>*:nth-child(2)]:mr-10 [&>*:nth-child(3)]:mr-8 [&>*:nth-child(4)]:mr-20 [&>*:nth-child(5)]:mr-2 md:[&>*:nth-child(5)]:mr-8">
-          <Logo className="w-10 md:w-40" />
+          <Logo className="w-20 md:w-40" />
 
           <div className="hidden md:block">
             <SelectBar />

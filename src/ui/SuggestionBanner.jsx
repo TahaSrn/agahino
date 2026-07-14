3e0ec22490baa1667390cf9837ba282fb5b42cff
TahaSrn <tahaahmadi0403@gmail.com>
2026-07-14 13:09:23 +0330

    Banner fixed

--- src/ui/SuggestionBanner.jsx
+++ src/ui/SuggestionBanner.jsx
@@ -1,11 +1,11 @@
 function SuggestionBanner() {
   return (
     <div className="mt-8 flex w-full justify-center md:mt-10">
-      <div className="w-[95%] md:w-[85%]">
+      <div className="w-[95%] overflow-hidden rounded-2xl md:w-[85%]">
         <img
           src="banner3.jpg"
           alt="agahino banner"
-          className="h-32 w-full rounded-2xl object-cover object-[center_45%] md:h-48 lg:h-56"
+          className="h-32 w-full scale-150 rounded-2xl object-cover object-[-250%_center] md:h-48 md:scale-100 md:object-[center_45%] lg:h-56"
         />
       </div>
     </div>
